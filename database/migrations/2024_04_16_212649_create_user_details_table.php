6cd32a1cc439774f1b27b96782e9fc1ab5812c6e
Kevin Sipahutar <Kevinsipahutar220604@gmail.com>
2024-04-16 22:09:24 +0700

    create user_details migration

---
+++ database/migrations/2024_04_16_212649_create_user_details_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->foreignUuid('user_id')->constrained('users', 'id')->noActionOnDelete();
+            $table->enum('gender', ['male', 'female'])->default('male');
+            $table->string('phone_number')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
