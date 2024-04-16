7a84a779c10e54a4c0d22701e0985e066da03c7c
Kevin Sipahutar <Kevinsipahutar220604@gmail.com>
2024-04-16 22:08:59 +0700

    create UserDetail model

---
+++ app/Models/UserDetail.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserDetail extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'gender',
+        'phone_number'
+    ];
+
+    public function userDetail(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
